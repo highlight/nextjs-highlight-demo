4e7ed6486c59f9de224ff91129d4b6be5bd5b4d0
Vadim Korolik <vkorolik@gmail.com>
2024-01-23 21:57:56 -0800

    update

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { logger, withHighlight } from '../../../highlight.config'
+import { logger } from '../../../highlight.config'
 
 type Data = {
 	name: string
@@ -18,4 +18,4 @@ function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 	res.status(200).json({ name: 'John Doe' })
 }
 
-export default withHighlight(handler)
+export default handler
